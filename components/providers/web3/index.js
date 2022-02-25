fc5ba33dfc7338b953fc085111a188e2b72c8ebc
papa <visidoor@yandex.ru>
2022-02-26 03:47:46 +0500

    22.11 (9.3) Use Hooks Abstraction

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -44,7 +44,7 @@ export default function Web3Provider({ children }) {
     return {
       ...web3Api,
       isWeb3Loaded: web3 != null,
-      hooks: setupHooks(web3),
+      getHooks: () => setupHooks(web3),
       connect: provider
         ? async () => {
             try {
@@ -67,4 +67,9 @@ export default function Web3Provider({ children }) {
 
 export function useWeb3() {
   return useContext(Web3Context);
+}
+
+export function useHooks(resolve) {
+  const { getHooks } = useWeb3();
+  return resolve(getHooks());
 }
