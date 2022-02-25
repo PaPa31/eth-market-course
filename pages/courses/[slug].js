7d1cec68eb0686515b2c3140c10a1176ffcd1c3a
papa <visidoor@yandex.ru>
2022-02-25 14:12:46 +0500

    21.6

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -1,10 +1,12 @@
 import { CourseHero, Curriculum, Keypoints } from "@components/course";
 import { Modal } from "@components/common";
 import { BaseLayout } from "@components/layout";
+import { getAllCourses } from "@content/courses/fetcher";
 
-export default function Course() {
+export default function Course({ course }) {
   return (
     <>
+      {course.title}
       <div className="py-4">
         <CourseHero />
       </div>
@@ -13,6 +15,30 @@ export default function Course() {
       <Modal />
     </>
   );
+}
+
+export function getStaticPaths() {
+  const { data } = getAllCourses();
+
+  return {
+    paths: data.map((c) => ({
+      params: {
+        slug: c.slug,
+      },
+    })),
+    fallback: false,
+  };
+}
+
+export function getStaticProps({ params }) {
+  const { data } = getAllCourses();
+  const course = data.filter((c) => c.slug === params.slug)[0];
+
+  return {
+    props: {
+      course,
+    },
+  };
 }
 
 Course.Layout = BaseLayout;
