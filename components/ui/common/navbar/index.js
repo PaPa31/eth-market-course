287132c9e87a1d847e574d3eda8a753feb65a7b6
papa <visidoor@yandex.ru>
2022-02-27 02:40:42 +0500

    23.9 Hide address on Marketplace

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -2,10 +2,12 @@ import { useWeb3 } from "@components/providers";
 import { Button } from "@components/ui/common";
 import Link from "next/link";
 import { useAccount } from "@components/hooks/web3/useAccount";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
   const { connect, isLoading, isWeb3Loaded } = useWeb3();
   const { account } = useAccount();
+  const { pathname } = useRouter();
 
   return (
     <section>
@@ -65,7 +67,7 @@ export default function Navbar() {
           </div>
         </nav>
       </div>
-      {account.data && (
+      {account.data && !pathname.includes("/marketplace") && (
         <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
           <div className="text-black bg-orange-200 rounded-md p-2">
             {account.data}
