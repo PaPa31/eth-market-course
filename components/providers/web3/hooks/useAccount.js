bda8d08fa866d411a6e902a75ff516d9492da9e5
papa <visidoor@yandex.ru>
2022-02-26 14:44:48 +0500

    23.6 Admin

--- components/providers/web3/hooks/useAccount.js
+++ components/providers/web3/hooks/useAccount.js
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
 import useSWR from "swr";
 
+const adminAddresses = {
+  "0x0eFd16312c8f0c1300780bE5B73a7baC8157d74B": true,
+};
+
 export const handler = (web3, provider) => () => {
-  const { mutate, ...rest } = useSWR(
+  const { data, mutate, ...rest } = useSWR(
     () => (web3 ? "web3/accounts" : null),
     async () => {
       const accounts = await web3.eth.getAccounts();
@@ -17,6 +21,8 @@ export const handler = (web3, provider) => () => {
 
   return {
     account: {
+      data,
+      isAdmin: (data && adminAddresses[data]) ?? false,
       mutate,
       ...rest,
     },
