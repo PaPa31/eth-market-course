d4112fa06efe816c02cc799d1b2f3c34c3d64065
papa <visidoor@yandex.ru>
2022-02-27 18:55:33 +0500

    24.2 Network changes

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -13,7 +13,7 @@ export default function Marketplace({ courses }) {
     <>
       <div className="py-4">
         {network.data}
-        <WalletBar address={account.data} />
+        <WalletBar address={account.data} network={network.data} />
       </div>
       <CourseList courses={courses} />
     </>
