34781286ee086ef9bfad38e6014b955696ada2c7
papa <visidoor@yandex.ru>
2022-03-03 20:10:15 +0500

    25.2 Course Card Styling

--- components/ui/course/card/index.js
+++ components/ui/course/card/index.js
@@ -5,27 +5,33 @@ export default function Card({ course, Footer }) {
   return (
     <div className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
       <div className="flex h-full">
-        <div className="flex h-full">
+        <div
+          className="flex-1 h-full next-image-wrapper"
+          width="200"
+          height="230"
+        >
           <Image
             className="object-cover"
             src={course.coverImage}
             alt={course.title}
-            layout="fixed"
+            layout="intrinsic"
             width="200"
             height="230"
           />
         </div>
-        <div className="p-8">
+        <div className="p-8 pb-4 flex-2">
           <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
             {course.type}
           </div>
           <Link href={`/courses/${course.slug}`}>
-            <a className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+            <a className="h-12 block mt-1 text-lg leading-tight font-medium text-black hover:underline">
               {course.title}
             </a>
           </Link>
 
-          <p className="mt-2 text-gray-500">{course.description}</p>
+          <p className="mt-2 text-gray-500">
+            {course.description.substring(0, 70)}...
+          </p>
           {Footer && <Footer />}
         </div>
       </div>
