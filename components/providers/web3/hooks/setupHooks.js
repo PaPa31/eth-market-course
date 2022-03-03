33e8c561ecff2cdb69ce28591592e797479e934c
papa <visidoor@yandex.ru>
2022-03-03 22:36:32 +0500

    25.6 Open Modal with Code

--- components/providers/web3/hooks/setupHooks.js
+++ components/providers/web3/hooks/setupHooks.js
@@ -2,7 +2,6 @@ import { handler as createAccountHook } from "./useAccount";
 import { handler as createNetworkHook } from "./useNetwork";
 
 export const setupHooks = (...deps) => {
-  console.log("Setting up hooks");
   return {
     useAccount: createAccountHook(...deps),
     useNetwork: createNetworkHook(...deps),
