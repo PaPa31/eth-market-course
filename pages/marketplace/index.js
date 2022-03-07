ab3f5b5c6d536d332975ca857bb5b9c66f990ade
papa <visidoor@yandex.ru>
2022-03-08 03:34:24 +0500

    29.6 Purchase Course

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -9,25 +9,12 @@ import { getAllCourses } from "@content/courses/fetcher";
 import { useState } from "react";
 
 export default function Marketplace({ courses }) {
-  const { web3 } = useWeb3();
+  const { web3, contract } = useWeb3();
   const { canPurchaseCourse, account } = useWalletInfo();
   const [selectedCourse, setSelectedCourse] = useState(null);
 
-  const purchaseCourse = (order) => {
+  const purchaseCourse = async (order) => {
     const hexCourseId = web3.utils.utf8ToHex(selectedCourse.id);
-    console.log(hexCourseId);
-
-    // hex course id
-    // 0x31333331363235000000000000000000
-
-    // address
-    // 0x0eFd16312c8f0c1300780bE5B73a7baC8157d74B
-
-    // 313333313632350000000000000000000eFd16312c8f0c1300780bE5B73a7baC8157d74B
-    // Order Hash
-    // 94442bb311fdbe3f78c960199a0bd820bc72d7c0da9a1cd35e353d021534f0eb
-
-    debugger;
     const orderHash = web3.utils.soliditySha3(
       {
         type: "bytes16",
@@ -35,16 +22,8 @@ export default function Marketplace({ courses }) {
       },
       { type: "address", value: account.data }
     );
-    console.log(orderHash);
     const emailHash = web3.utils.sha3(order.email);
 
-    // greek@reka.gr
-    // 1589b84f652605e2f037ababe860edba0639b7ce58139718d8396328986a3f01
-    console.log(emailHash);
-
-    // 1589b84f652605e2f037ababe860edba0639b7ce58139718d8396328986a3f0194442bb311fdbe3f78c960199a0bd820bc72d7c0da9a1cd35e353d021534f0eb
-    // proof:
-    // 1529daeb5660add57f8ac76bad78d6185ebf076ee393dc1fe6174b993fcafa9d
     const proof = web3.utils.soliditySha3(
       {
         type: "bytes32",
@@ -55,7 +34,17 @@ export default function Marketplace({ courses }) {
         value: orderHash,
       }
     );
-    console.log(proof);
+
+    const value = web3.utils.toWei(String(order.price));
+
+    try {
+      const result = await contract.methods
+        .purchaseCourse(hexCourseId, proof)
+        .send({ from: account.data, value });
+      console.log(result);
+    } catch {
+      console.error("Purchase course: Operation has failed!");
+    }
   };
 
   return (
