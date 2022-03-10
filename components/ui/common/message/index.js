52094cacf8bcc3a3d35881f649fc44643082bd81
papa <visidoor@yandex.ru>
2022-03-11 00:37:42 +0500

    32.3 Course state messages

--- components/ui/common/message/index.js
+++ components/ui/common/message/index.js
@@ -1,9 +1,14 @@
 import { useState } from "react";
 
-const TYPES = {
-  success: "green",
-  warning: "yellow",
-  danger: "red",
+const TYPES_BG = {
+  success: "bg-green-100",
+  warning: "bg-yellow-100",
+  danger: "bg-red-100",
+};
+const TYPES_TEXT = {
+  success: "text-green-900",
+  warning: "text-yellow-900",
+  danger: "text-red-900",
 };
 
 export default function Message({ children, type = "success" }) {
@@ -13,14 +18,15 @@ export default function Message({ children, type = "success" }) {
     return null;
   }
 
-  const messageType = TYPES[type];
+  const messageTypeBg = TYPES_BG[type];
+  const messageTypeText = TYPES_TEXT[type];
 
   return (
-    <div className={`bg-${messageType}-100 rounded-xl mb-3`}>
+    <div className={`${messageTypeBg} rounded-xl mb-3`}>
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-3 lg:px-3">
         <div className="flex items-center justify-between flex-wrap">
           <div className="w-0 flex-1 flex items-center">
-            <p className={`ml-3 font-medium text-${messageType}-900 truncate`}>
+            <p className={`ml-3 font-medium ${messageTypeText}`}>
               <span className="hidden md:inline">{children}</span>
             </p>
           </div>
@@ -32,7 +38,7 @@ export default function Message({ children, type = "success" }) {
             >
               <span className="sr-only">Dismiss</span>
               <svg
-                className={`h-6 w-6 text-${messageType}-900`}
+                className={`h-6 w-6 ${messageTypeText}`}
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
