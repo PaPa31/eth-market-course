a785cc01f5d872164de263d6fe70046f5c704848
papa <visidoor@yandex.ru>
2022-03-11 16:50:37 +0500

    32.7 Check if empty account

--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -1,4 +1,5 @@
 import { useAccount, useOwnedCourses } from "@components/hooks/web3";
+import { useWeb3 } from "@components/providers";
 import { Button, Message } from "@components/ui/common";
 import { OwnedCourseCard } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
@@ -9,6 +10,7 @@ import { useRouter } from "next/router";
 
 export default function OwnedCourses({ courses }) {
   const router = useRouter();
+  const { requireInstall } = useWeb3();
   const { account } = useAccount();
   const { ownedCourses } = useOwnedCourses(courses, account.data);
   return (
@@ -18,12 +20,26 @@ export default function OwnedCourses({ courses }) {
         {ownedCourses.isEmpty && (
           <div className="w-1/2">
             <Message type="warning">
-              <div>You don't have any courses</div>
+              <div>You don't own any courses</div>
               <Link href="/marketplace">
                 <a className="font-normal hover:underline">
                   <i>Purchase Course</i>
                 </a>
               </Link>
+            </Message>
+          </div>
+        )}
+        {account.isEmpty && (
+          <div className="w-1/2">
+            <Message type="warning">
+              <div>Please connect to Metamask</div>
+            </Message>
+          </div>
+        )}
+        {requireInstall && (
+          <div className="w-1/2">
+            <Message type="warning">
+              <div>Please install Metamask</div>
             </Message>
           </div>
         )}
@@ -48,5 +64,4 @@ export function getStaticProps() {
     },
   };
 }
-
 OwnedCourses.Layout = BaseLayout;
