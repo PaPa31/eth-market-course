a7df5e1043643a074f98db5e7e42cade52264549
papa <visidoor@yandex.ru>
2022-03-11 18:59:32 +0500

    32.9 One time event

--- components/providers/web3/hooks/useAccount.js
+++ components/providers/web3/hooks/useAccount.js
@@ -22,13 +22,12 @@ export const handler = (web3, provider) => () => {
   );
 
   useEffect(() => {
-    console.log("SUBSCRIBING TO EVENT");
-    provider &&
-      provider.on("accountsChanged", (accounts) => {
-        console.log("ON ACCOUNT DATA");
-        mutate(accounts[0] ?? null);
-      });
-    console.log(provider);
+    const mutator = (accounts) => mutate(accounts[0] ?? null);
+    provider?.on("accountsChanged", mutator);
+
+    return () => {
+      provider?.removeListener("accountsChanged", mutator);
+    };
   }, [provider]);
 
   return {
