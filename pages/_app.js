25db8a6f67783362379686d61a341034f03a65ed
papa <visidoor@yandex.ru>
2022-03-31 05:04:01 -0400

    41.2 React Toastify

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "@styles/globals.css";
 
 const Noop = ({ children }) => <>{children}</>;
@@ -6,6 +8,7 @@ function MyApp({ Component, pageProps }) {
   const Layout = Component.Layout ?? Noop;
   return (
     <Layout>
+      <ToastContainer />
       <Component {...pageProps} />
     </Layout>
   );
