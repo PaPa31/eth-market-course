40ccbfd9f4807d52c9d65186a29b8f817eda522d
papa <visidoor@yandex.ru>
2022-04-12 03:37:57 -0400

    42.2 (Initially "Fix Colors"): In my case: Change NETWORK_ID to 3;
    add Purge to tailwind.config.css for more style file compress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+	purge: {
+		enabled: true,
+		content: [
+			"./pages/**/*.{js,ts,jsx,tsx}",
+			"./components/**/*.{js,ts,jsx,tsx}",
+		],
+	},
 	theme: {
 		screens: {
 			"xs": "475px",
